d2674ea1fd8413b72316a87d0096f281babe7a5a
Sandor Szuecs <sandor.szuecs@zalando.de>
2016-05-10 16:50:30 +0200

    better example to show the difference of byte and rune

--- go-class/src/basics/basics.go
+++ go-class/src/basics/basics.go
@@ -7,10 +7,11 @@ var x float64 // package global
 func main() {
 	var y, z float64 // function local
 	x = 3.0 + y
-	fmt.Println("x:", x, ", z:", z)
+	fmt.Println("x:", x, ", y:", y, ", z:", z)
 
 	const str string = "this is a 日本語 string\n"
-	var ch byte = str[11]
+	var ch byte = str[12]
 	r := []rune(str)
-	fmt.Printf("ch: %q, rune: %q\n", ch, r[11])
+	fmt.Printf("ch: %q, rune: %q\n", ch, r[12])
+	fmt.Printf("ch  : %[1]q/%[1]v/%016[1]b\nrune: %[2]q/%[2]v/%016[2]b\n", ch, r[12])
 }
